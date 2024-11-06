f0d2b60c25aa59f6be58a7067d607ee0333c8722
Hammas2580 <hasirali653@gmail.com>
2024-11-06 21:55:27 +0500

    chapter no 26 to 30 complete

--- chapter no 26 to 30 MATH METHODS/app.js
+++ chapter no 26 to 30 MATH METHODS/app.js
@@ -2,6 +2,70 @@
 
 //  ============= question no 1 ==================
 
-var num = 3.45214;
-var res = Math.round(3.45214);
-console.log("Round off value " + " " + res);
+// var num = 3.45214;
+// var res = Math.round(3.45214);
+// console.log("Round off value " + " " + res);
+
+// var num = 3.45214;
+//  var res  = Math.ceil(3.545564);
+// console.log("floor value " + "" + res);
+
+// var num = 3.45214;
+// var res = Math.floor(3.45214);
+// console.log(res);
+
+//  ============= question no 2 ==================
+
+// var user = prompt("Enter your number");
+// var num = parseFloat(user);
+
+// var roundValue = Math.round(num);
+// var florrValue = Math.floor(num);
+// var ceilValue = Math.ceil(num);
+
+// console.log("this is round value" + " " + roundValue);
+// console.log("this is floor value" + " " + roundValue);
+// console.log("this is ceil value" + " " + roundValue);
+
+// =============== question no 3 ====================
+
+// let value = prompt("choose any one number");
+// let absoluteValue = Math.abs(value);
+// console.log(absoluteValue);
+
+// =============== question no 4 ====================
+
+// function game() {
+//   let user1 = (prompt("Enter  your First player name"));
+//   let user2 = (prompt("Enter  your second player name"));
+//   let toss = (Math.floor(Math.random) * 2);
+//   if (user1 == 0){
+//     console.log(user1 + " "  + " win the match")
+//   } else {
+//     console.log(user2 + "" + " win the match")
+//   }
+  
+  
+// }
+// game();
+
+// =============== question no 5 ====================
+
+// function randomNumber(){
+//     var num = 100;
+//     var ran = (Math.floor(Math.random() * num) + 1)
+//     document.write("Random nunber between 1 to 100 : " + "" + ran);;
+// }
+// randomNumber();
+
+// =============== question no 6 ====================
+
+
+// var userInput = Number(prompt("Enter number between 1 to 10"));
+// var res = Math.round(Math.random() * 10) + 1;
+// console.log(res);
+// if(userInput == res ){
+//     console.log("congrulation your are correct")
+// } else {
+//     console.log("soory");
+// }
